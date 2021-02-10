731e2f043ae0e5d37f23f8d1c9fe5f22780f2854
ranyoung <fksdud456@gmail.com>
2021-02-10 13:56:47 +0900

    test

--- KiwoomTrade.py
+++ KiwoomTrade.py
@@ -13,6 +13,7 @@ class KiwoomTrade(QAxWidget, SystemTrade):
     # rqlist = {
     #     'opt10081_req': self._opt10081()
     # }
+    stock_list = []
 
     def __init__(self):
         super().__init__()
@@ -26,6 +27,7 @@ class KiwoomTrade(QAxWidget, SystemTrade):
         self.OnEventConnect.connect(self._event_connect)
         self.OnReceiveTrData.connect(self._receive_tr_data)
         self.OnReceiveChejanData.connect(self._receive_chejan_data)
+        self.OnReceiveRealData.connect(self._receive_real_data)
 
     def comm_connect(self):
         self.dynamicCall("CommConnect()")
@@ -79,6 +81,11 @@ class KiwoomTrade(QAxWidget, SystemTrade):
                                real_type, field_name, index, item_name)
         return ret.strip()
 
+    def _comm_real_get_data(self, code, real_type, field_name, index, item_name):
+        ret = self.dynamicCall("GetCommRealData(QString, QString, QString, int, QString)", code,
+                               real_type, field_name, index, item_name)
+        return ret.strip()
+
     def _get_repeat_cnt(self, trcode, rqname):
         ret = self.dynamicCall("GetRepeatCnt(QString, QString)", trcode, rqname)
         return ret
@@ -88,13 +95,12 @@ class KiwoomTrade(QAxWidget, SystemTrade):
             self.remained_data = True
         else:
             self.remained_data = False
-
         exec('self._' + rqname.split('_')[0] + '(rqname, trcode)')
 
-        try:
-            self.tr_event_loop.exit()
-        except AttributeError:
-            pass
+        # try:
+        #     self.tr_event_loop.exit()
+        # except AttributeError:
+        #     pass
 
     def _receive_chejan_data(self, gubun, item_cnt, fid_list):
         print('_receive_chejan_data')
@@ -103,6 +109,22 @@ class KiwoomTrade(QAxWidget, SystemTrade):
         print(self.get_chejan_data(302))
         print(self.get_chejan_data(900))
         print(self.get_chejan_data(901))
+
+    def _receive_real_data(self, screen_no, rqname, trcode, record_name, next, unused1, unused2, unused3, unused4):
+        if next == '2':
+            self.remained_data = True
+        else:
+            self.remained_data = False
+
+        if rqname == "opt10081_req":
+            self._opt10081(rqname, trcode)
+        elif rqname == "opw00001_req":
+            self._opw00001(rqname, trcode)
+
+        try:
+            self.tr_event_loop.exit()
+        except AttributeError:
+            pass
 
     def _opw00001(self, rqname, trcode):
         self.d2_deposit = self._comm_get_data(trcode, "", rqname, 0, "d+2추정예수금")
@@ -125,9 +147,68 @@ class KiwoomTrade(QAxWidget, SystemTrade):
 
             print(name, quantity, purchase_price, current_price, eval_profit_loss_price, earning_rate)
 
+
+    def get_current_price(self, code):
+        # 호가요청
+        self.set_input_value("종목코드", code)
+        self.comm_rq_data("opt10004_req", "opt10004", 0, "2000")
+
+        # 현재가
+        self.set_input_value("종목코드", code)
+        self.comm_rq_data("opt10001_req", "opt10001", 0, "2000")
+
+    # get_current_price
+    def _opt10004(self, rqname, trcode):
+        rows = self._get_repeat_cnt(trcode, rqname)
+        for i in range(rows):
+            h1 = self._comm_get_data(trcode, "", rqname, i, "매도최우선호가")
+            h2 = self._comm_get_data(trcode, "", rqname, i, "매수최우선호가")
+            print(h1, h2)
+
     def _opt10001(self, rqname, trcode):
         print(self._comm_get_data(trcode, "", rqname, 0, "현재가"))
         print(self._comm_get_data(trcode, "", rqname, 0, "종목명"))
+        print(self._comm_get_data(trcode, "", rqname, 0, "PER"))
+        self.stock_list.append({})
+        try:
+            self.tr_event_loop.exit()
+        except AttributeError:
+            pass
+
+    # 이건 한 번만 호출해서 theme 별, 종목코드 다 저장해두고
+    # 내 종목코드가 어떤 테마에 속하는지 db에서 검색해서 가져오면 될 듯
+    # db에 있을건 내 종목의 .. 그거랑
+    # theme 관련된건 다른 db
+    def get_sector_list(self):
+        # 테마 코드와 그룹의 리스트를 반환한다
+        # 반환값의
+        # 코드와 코드명 구분은 ‘ | ’
+        # 코드의 구분은 ‘;’
+        # Ex) 100 | 태양광_폴리실리콘; 152 | 합성섬유
+        ret = self.dynamicCall("GetThemeGroupList(int)", 0)  # 0: 코드>명
+        code = ret.split(';')
+        sector_dict = {}
+        for i in code:
+            sector_list = i.split('|')
+            sector_dict.update({sector_list[0]: sector_list[1]})
+
+        return sector_dict
+
+    def get_sector_group_code(self, sector_list):
+        """
+        :param sector_list: list of sector code
+        :return: ticker, sector(code) dict
+        """
+        print(sector_list)
+        ticker_list = []
+        for sector in sector_list:
+            # 테마코드에 해당하는 종목코드를 반환한다.
+            # 반환값의 종목코드간 구분은 ‘;’
+            ret = self.dynamicCall("GetThemeGroupCode(QString)", sector)
+            for ticker in ret.split(';'):
+                # ticker_dict.update({'ticker': ticker, 'sector': sector})
+                ticker_list.append({'ticker': ticker, 'sector': sector})
+        return ticker_list
 
 
 if __name__ == "__main__":
@@ -143,31 +224,59 @@ if __name__ == "__main__":
     account_number = kiwoom.get_login_info("ACCNO")
     account_number = account_number.split(';')[0]
 
-    # d2 예수금
-    kiwoom.set_input_value("계좌번호", account_number)
-    kiwoom.set_input_value("비밀번호", account.kiwoom['simple_pw'])
-    kiwoom.comm_rq_data("opw00001_req", "opw00001", 0, "2000")
-    print(kiwoom.d2_deposit)
-
-    # 총매입금액, 총평가금액, 총평가손익금액, 총수익률, 추정예탁자산을
-    kiwoom.set_input_value("계좌번호", account_number)
-    kiwoom.comm_rq_data("opw00018_req", "opw00018", 0, "2000")
-
-    # 현재가
-    kiwoom.set_input_value("종목코드", 'A122630')
-    kiwoom.comm_rq_data("opt10001_req", "opt10001", 0, "2000")
+    # # d2 예수금
+    # kiwoom.set_input_value("계좌번호", account_number)
+    # kiwoom.set_input_value("비밀번호", account.kiwoom['simple_pw'])
+    #
+    # kiwoom.comm_rq_data("opw00001_req", "opw00001", 0, "2000")
+    # print(kiwoom.d2_deposit)
+    #
+    # # 총매입금액, 총평가금액, 총평가손익금액, 총수익률, 추정예탁자산을
+    # kiwoom.set_input_value("계좌번호", account_number)
+    # kiwoom.comm_rq_data("opw00018_req", "opw00018", 0, "2000")
+    #
+    # # 현재가
+    # kiwoom.set_input_value("종목코드", 'A122630')
+    # kiwoom.comm_rq_data("opt10001_req", "opt10001", 0, "2000")
 
     # 주문
-    kiwoom.send_order(rqname='send_order_req',
-                      screen_no='0101',
-                      acc_no=account_number,
-                      # {'신규매수': 1, '신규매도': 2, '매수취소': 3, '매도취소': 4}
-                      order_type=1,
-                      # 종목코드
-                      code='000660',
-                      # 주문 갯수
-                      quantity=10,
-                      price=0,
-                      # hoga {'지정가': "00", '시장가': "03"}
-                      hoga='03',
-                      order_no="")
+    # kiwoom.send_order(rqname='send_order_req',
+    #                   screen_no='0101',
+    #                   acc_no=account_number,
+    #                   # {'신규매수': 1, '신규매도': 2, '매수취소': 3, '매도취소': 4}
+    #                   order_type=1,
+    #                   # 종목코드
+    #                   code='000660',
+    #                   # 주문 갯수
+    #                   quantity=10,
+    #                   price=0,
+    #                   # hoga {'지정가': "00", '시장가': "03"}
+    #                   hoga='03',
+    #                   order_no="")
+
+    sector_dict = kiwoom.get_sector_list()
+    ticker_list = kiwoom.get_sector_group_code(sector_dict.keys())
+    print('ticker_list: ', ticker_list)
+    a = 0
+    # 전체를 다 update할 필요가 있는지 모르겠네.. 내 종목 중에서만
+    for ticker in ticker_list:
+        # 'ticker': ticker, 'sector': sector
+        print(ticker)
+        kiwoom.set_input_value("종목코드", ticker['ticker'])
+        kiwoom.comm_rq_data("opt10001_req_" + ticker['ticker'], "opt10001", 0, "2000")
+        a += 1
+        if a == 5:
+            break
+    print(kiwoom.stock_list)
+
+    # ticker(primary)
+    # sector(code)
+    # name( = ticker)
+    # dividend
+    # 배당금(1 회)
+    # period(Y / Q / M)
+    # dividend
+    # yield (배당률? %)
+    # PER
+    # 월평균성장률( %)?
+    # criteria(성장 / 테마 / 배당 / 배당서앙 / 가치
